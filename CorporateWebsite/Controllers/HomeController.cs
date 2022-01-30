a6710092bb002338ee725cd60f6d8cc924a0b481
Alpisoren <67542993+Alpisoren@users.noreply.github.com>
2022-01-30 21:59:44 +0300

    Anasayfa düzenlemeleri

--- CorporateWebsite/Controllers/HomeController.cs
+++ CorporateWebsite/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CorporateWebsite.Models.DataContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,29 @@ namespace CorporateWebsite.Controllers
 {
     public class HomeController : Controller
     {
+
+
+        private KurumsalDBContext db = new KurumsalDBContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(db.Slider.ToList().OrderByDescending(x => x.SliderId));
         }
+
+
+       
+        [Obsolete("Silinecek")]
+        public ActionResult SliderPartial()
+        {
+            return View(db.Slider.ToList().OrderByDescending(x=>x.SliderId));
+        }
+
+        public ActionResult HizmetPartipal()
+        {
+            return View(db.Hizmet.ToList().OrderByDescending(x=>x.HizmetId));
+
+        }
+
+        
     }
 }
